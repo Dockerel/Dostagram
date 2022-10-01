83393ac4cc19b407c7b3d3f6f978d4da3eeea11d
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-10-01 15:50:45 +0900

    fucking image slide func  added

--- src/client/js/postMovement.js
+++ src/client/js/postMovement.js
@@ -1,51 +1,80 @@
+let pageNumberList = new Array();
+const postContainerNumber =
+  document.getElementsByClassName("post-container").length;
+
+const containerIds = document.querySelectorAll(".container");
+containerIds.forEach((containerId) => {
+  const tempId = containerId.id;
+  let processedId = tempId.replace("Container", "");
+
+  let data = new Object();
+
+  data.id = processedId;
+  data.pageNumber = 0;
+
+  pageNumberList.push(data);
+});
+
 const buttonLefts = document.querySelectorAll(".btnLeft");
 const buttonRights = document.querySelectorAll(".btnRight");
-const photoContainer = document.querySelectorAll(".container");
-let pageNumber = [];
-let objectNumber;
 
-const handleLeftClick = (objectNumber) => {
-  const pixelLeftVar = 350 * (pageNumber[objectNumber] - 1);
-  photoContainer[
-    objectNumber
-  ].style.transform = `translate(-${pixelLeftVar}px)`;
-  if (pageNumber[objectNumber] === 1) {
-    pageNumber[objectNumber] = 1;
-  } else {
-    pageNumber[objectNumber] = pageNumber[objectNumber] - 1;
+let pageNumber = 0;
+let targetListNumber = 0;
+
+const handleLeftClick = (event) => {
+  const id = event.target.parentNode.id;
+  let parsedId = id.replace("LeftBtn", "");
+
+  const photoContainerId = `${parsedId}Container`;
+  const photoContainer = document.getElementById(photoContainerId);
+  const photoContainerLength =
+    photoContainer.getElementsByTagName("img").length;
+
+  for (let i = 0; i < postContainerNumber; i++) {
+    if (parsedId === pageNumberList[i].id) {
+      pageNumber = pageNumberList[i].pageNumber;
+      targetListNumber = i;
+    }
   }
+  const pixelLeftVar = 350 * (pageNumber - 1);
+  photoContainer.style.transform = `translate(-${pixelLeftVar}px)`;
+  if (pageNumber === 0) {
+    pageNumber = 0;
+  } else {
+    pageNumber = pageNumber - 1;
+  }
+  pageNumberList[targetListNumber].pageNumber = pageNumber;
 };
 
-function handleRightClick(objectNumber) {
-  const pixelRightVar = 350 * pageNumber[objectNumber];
-  photoContainer[
-    objectNumber
-  ].style.transform = `translate(-${pixelRightVar}px)`;
-  if (pageNumber[objectNumber] === photoContainer[objectNumber].length - 1) {
-    pageNumber[objectNumber] = photoContainer[objectNumber].length - 1;
-  } else {
-    pageNumber[objectNumber] = pageNumber[objectNumber] + 1;
+const handleRightClick = (event) => {
+  const id = event.target.parentNode.id;
+  let parsedId = id.replace("RightBtn", "");
+
+  const photoContainerId = `${parsedId}Container`;
+  const photoContainer = document.getElementById(photoContainerId);
+  const photoContainerLength =
+    photoContainer.getElementsByTagName("img").length;
+
+  for (let i = 0; i < postContainerNumber; i++) {
+    if (parsedId === pageNumberList[i].id) {
+      pageNumber = pageNumberList[i].pageNumber;
+      targetListNumber = i;
+    }
   }
-}
 
-// for (const buttonLeft of buttonLefts) {
-//   buttonLeft.addEventListener("click", handleLeftClick);
-// }
-// for (const buttonRight of buttonRights) {
-//   buttonRight.addEventListener("click", handleRightClick);
-// }
+  const pixelRightVar = 350 * (pageNumber + 1);
+  if (pageNumber === photoContainerLength - 1) {
+    pageNumber = photoContainerLength - 1;
+  } else {
+    photoContainer.style.transform = `translate(-${pixelRightVar}px)`;
+    pageNumber = pageNumber + 1;
+  }
+  pageNumberList[targetListNumber].pageNumber = pageNumber;
+};
 
-for (var i = 0; i < photoContainer.length; i++) {
-  pageNumber.push(1);
-}
-console.log(pageNumber);
-
-for (var i = 0; i < buttonLefts.length; i++) {
-  let hiddenNb = i;
-  //   buttonLefts[i].addEventListener("click", handleLeftClick(i));
-  buttonLefts[i].addEventListener("click", console.log("pass"));
-}
-
-for (var i = 0; i < buttonRights.length; i++) {
-  buttonRights[i].addEventListener("click", handleRightClick(i));
-}
+buttonLefts.forEach((buttonLeft) => {
+  buttonLeft.addEventListener("click", handleLeftClick);
+});
+buttonRights.forEach((buttonRight) => {
+  buttonRight.addEventListener("click", handleRightClick);
+});
