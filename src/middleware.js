028ddee9be0a194334aaa7586227cd14852efdd9
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-10-12 18:02:44 +0900

    storage setting updated

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,33 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3AvatarUploader = multerS3({
+  s3: s3,
+  bucket: "dostagram/avatar",
+  acl: "public-read",
+});
+
+const s3PostImageUploader = multerS3({
+  s3: s3,
+  bucket: "dostagram/postImage",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "dostagram/video",
+  acl: "public-read",
+});
 
 export const localMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -27,14 +56,17 @@ export const publicUserOnly = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatar",
   limits: { fileSize: 3000000 },
+  storage: isHeroku ? s3AvatarUploader : undefined,
 });
 
 export const postImageUpload = multer({
   dest: "uploads/postImages",
   limits: { fileSize: 10000000 },
+  storage: isHeroku ? s3PostImageUploader : undefined,
 });
 
 export const videoUpload = multer({
   dest: "uploads/videos",
   limits: { fileSize: 10000000 },
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
