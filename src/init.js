d2aed421794db44465969740793d327b8ce8ec39
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-10-12 17:10:26 +0900

    port and varilable

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./models/User.js";
 import "./models/Post.js";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => {
   console.log(`✅ Server Listening on port ${PORT} | http://localhost:${PORT}`);
